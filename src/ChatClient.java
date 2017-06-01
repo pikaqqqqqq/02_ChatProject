d36ea03ce91f1b3d9e5ea40a3ab1abc546b67d02
pikaqqqqq <281705236@qq.com>
2017-06-01 12:35:49 +0800

    Initial commit

---
+++ src/ChatClient.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+/**
+ * Created by think on 2017/5/29.
+ */
+public class ChatClient extends Frame {
+
+    public static void main(String[] args){
+        new ChatClient().launchFrame();
+    }
+
+    public void launchFrame(){
+        setSize(500,500);
+        setLocation(500,150);
+        setVisible(true);
+    }
+}
